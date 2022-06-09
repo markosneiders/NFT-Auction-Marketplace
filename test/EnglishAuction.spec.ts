69fb1e0d35009b2edc0453459b98b7dc152524d1
Zappingmadnnes <roberts.mezals@gmail.com>
2022-06-09 14:29:32 +0300

    Test moving along

--- test/EnglishAuction.spec.ts
+++ test/EnglishAuction.spec.ts
@@ -7,6 +7,7 @@ import config from "./vite.config.json"
 import * as vite from "@vite/vuilder/lib/vite"
 import { Contract } from "@vite/vuilder/lib/contract"
 import { UserAccount } from "@vite/vuilder/lib/user"
+import { Address } from "cluster"
 
 chai.use(bnChai(BN))
 chai.use(cap)
@@ -23,8 +24,23 @@ describe("test EnglishAuction", () => {
   let ERC721: any
 
   let nft: string
-  let nftId: string
-  let startingBid: string
+  let nftId: string = "0"
+
+  let blankAddress: string = "vite_0000000000000000000000000000000000000000a4f3a0cb58"
+
+  async function deployEnglishAuction(_nftId: string, _startingBid: string) {
+    // compile
+    const compiledEnglishAuction = await vuilder.compile("EnglishAuction.solpp")
+    expect(compiledEnglishAuction).to.have.property("EnglishAuction")
+
+    // deploy
+    englishAuction = compiledEnglishAuction.EnglishAuction
+    englishAuction.setDeployer(deployer).setProvider(provider)
+    await englishAuction.deploy({ responseLatency: 1, params: [nft, _nftId, _startingBid] })
+    expect(englishAuction.address).to.be.a("string")
+    console.log(englishAuction.address)
+    return englishAuction
+  }
 
   before(async function () {
     //Deployer and provider definitions
@@ -34,13 +50,11 @@ describe("test EnglishAuction", () => {
     buyer2 = vite.newAccount(config.networks.local.mnemonic, 2, provider)
     buyer3 = vite.newAccount(config.networks.local.mnemonic, 3, provider)
     buyer4 = vite.newAccount(config.networks.local.mnemonic, 4, provider)
-    //Deploy ERC721
 
-    // compile
+    //Compile and deploy ERC721 for testing pourposes
     const compiledERC721 = await vuilder.compile("ERC721.solpp")
     expect(compiledERC721).to.have.property("ERC721")
 
-    // deploy
     ERC721 = compiledERC721.ERC721
     ERC721.setDeployer(deployer).setProvider(provider)
     await ERC721.deploy({ responseLatency: 1 })
@@ -48,32 +62,19 @@ describe("test EnglishAuction", () => {
     console.log(ERC721.address)
 
     nft = ERC721.address
-    nftId = "77"
-  })
-  beforeEach(async function () {
-    // compile
-    const compiledEnglishAuction = await vuilder.compile("EnglishAuction.solpp")
-    expect(compiledEnglishAuction).to.have.property("EnglishAuction")
-
-    startingBid = "1"
-
-    // deploy
-    englishAuction = compiledEnglishAuction.EnglishAuction
-    englishAuction.setDeployer(deployer).setProvider(provider)
-    await englishAuction.deploy({
-      responseLatency: 1,
-      params: [nft, nftId, startingBid],
-    })
-    expect(englishAuction.address).to.be.a("string")
-    console.log(englishAuction.address)
   })
 
   it("Contract initialization values are correct", async () => {
-    await englishAuction.query("bids", ["vite_0000000000000000000000000000000000000000a4f3a0cb58"]).should.eventually.deep.equal(["0"])
+    //deploy
+    nftId = String(Number(nftId) + 1)
+    let startingBid = "1"
+    englishAuction = await deployEnglishAuction(nftId, startingBid)
+
+    await englishAuction.query("bids", [blankAddress]).should.eventually.deep.equal(["0"])
     await englishAuction.query("endAt", []).should.eventually.deep.equal(["0"])
     await englishAuction.query("ended", []).should.eventually.deep.equal(["0"])
     await englishAuction.query("highestBid", []).should.eventually.deep.equal([startingBid])
-    await englishAuction.query("highestBidder", []).should.eventually.deep.equal(["vite_0000000000000000000000000000000000000000a4f3a0cb58"])
+    await englishAuction.query("highestBidder", []).should.eventually.deep.equal([blankAddress])
     await englishAuction.query("nft", []).should.eventually.deep.equal([nft])
     await englishAuction.query("nftId", []).should.eventually.deep.equal([nftId])
     await englishAuction.query("seller", []).should.eventually.deep.equal([deployer.address])
@@ -81,8 +82,35 @@ describe("test EnglishAuction", () => {
   })
 
   it("Start function works", async () => {
+    //deploy
+    nftId = String(Number(nftId) + 1)
+    let startingBid = "1"
+    englishAuction = await deployEnglishAuction(nftId, startingBid)
+
     //create NFT for deployer to sell
     await ERC721.call("mint", [deployer.address, nftId], { caller: deployer })
-    console.log(await ERC721.query("ownerOf", [nftId]))
+    await ERC721.call("approve", [englishAuction.address, nftId], { caller: deployer })
+
+    //Verify NFT owenership and approval
+    await ERC721.query("ownerOf", [nftId]).should.eventually.deep.equal([deployer.address])
+    await ERC721.query("getApproved", [nftId]).should.eventually.deep.equal([englishAuction.address])
+
+    //Start auction
+    await englishAuction.call("start", [], { caller: deployer })
+
+    //Verify contract values
+    await englishAuction.query("bids", [blankAddress]).should.eventually.deep.equal(["0"])
+    await englishAuction.query("endAt", []).should.not.eventually.deep.equal(["0"])
+    await englishAuction.query("ended", []).should.eventually.deep.equal(["0"])
+    await englishAuction.query("highestBid", []).should.eventually.deep.equal([startingBid])
+    await englishAuction.query("highestBidder", []).should.eventually.deep.equal([blankAddress])
+    await englishAuction.query("nft", []).should.eventually.deep.equal([nft])
+    await englishAuction.query("nftId", []).should.eventually.deep.equal([nftId])
+    await englishAuction.query("seller", []).should.eventually.deep.equal([deployer.address])
+    await englishAuction.query("started", []).should.eventually.deep.equal(["1"])
+
+    //Verify NFT ownership
+    await ERC721.query("ownerOf", [nftId]).should.eventually.deep.equal([englishAuction.address])
+    // await ERC721.query("getApproved", [nftId]).should.eventually.deep.equal([blankAddress])
   })
 })
